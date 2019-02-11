691c1162b929ea58c7ecfc41e91096d6e8ead242
fagnersutel <fagnersutel@hotmail.com>
2019-02-11 20:14:26 -0200

    Add files via upload

---
+++ Consulta_Distancia_SQL_R_tcc.R
@@ -0,0 +1,16 @@
+setwd("~/OneDrive/r-files/AffinityPropagationClustering/")
+filenames <- list.files(path = "~/OneDrive/r-files/AffinityPropagationClustering/geo/") 
+filenames
+setwd("~/OneDrive/r-files/AffinityPropagationClustering/geo/") 
+data <- do.call("rbind", lapply(filenames, read.csv, header = TRUE, sep = ";")) 
+setwd("~/OneDrive/r-files/AffinityPropagationClustering/")
+head(data)
+names(data)
+library(geosphere)
+library(sqldf)
+coord_sub_set = sqldf("SELECT *, (6371 * acos(cos( radians(-30.053831)) * cos(radians(lat)) * cos(radians(long) - radians(-51.191810)) + sin(radians(-30.053831)) * sin(radians(lat)))) AS distancia FROM data GROUP BY lat HAVING distancia < 0.2 ORDER BY distancia ASC ")
+dim(data)
+dim(coord_sub_set)
+coord_sub_set
+distm(c(-30.053831, -51.191810), c(-30.05412, -51.19247), fun = distHaversine)
+View(coord_sub_set)

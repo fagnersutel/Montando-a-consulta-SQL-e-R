28263d91f70b300df9810a2c932111db2d10c99f
fagnersutel <fagnersutel@hotmail.com>
2019-02-11 20:14:11 -0200

    Add files via upload

--- Consulta_Distancia_SQL_R.R
+++ Consulta_Distancia_SQL_R.R
@@ -30,6 +30,7 @@ coord_df
 
 #https://stackoverflow.com/questions/32363998/function-to-calculate-geospatial-distance-between-two-points-lat-long-using-r
 library(geosphere)
+library(sqldf)
 distm(c(-2.106472, 57.14455), c(-2.104092, 57.14485), fun = distHaversine)
 
 
@@ -54,14 +55,102 @@ distm(c(-2.106472, 57.14455), c(-2.249217, 57.10063), fun = distHaversine)
 
 
 
-
+setwd("~/OneDrive/r-files/AffinityPropagationClustering/")
+filenames <- list.files(path = "~/OneDrive/r-files/AffinityPropagationClustering/geo/") 
+filenames
+setwd("~/OneDrive/r-files/AffinityPropagationClustering/geo/") 
+data <- do.call("rbind", lapply(filenames, read.csv, header = TRUE, sep = ";")) 
+setwd("~/OneDrive/r-files/AffinityPropagationClustering/")
 head(data)
-coord_sub_set = sqldf("SELECT *, (6371 * acos(cos( radians(-30.053831)) * cos(radians(lat)) * cos(radians(long) - radians(-51.191810)) + sin(radians(-30.053831)) * sin(radians(lat)))) AS distancia FROM data GROUP BY lat HAVING distancia < 1 ORDER BY distancia ASC ")
+names(data)
+head(data)
+data1 = data[, 1:2]
+coord_sub_set = sqldf("SELECT *, (6371 * acos(cos( radians(-30.053831)) * cos(radians(lat)) * cos(radians(long) - radians(-51.191810)) + sin(radians(-30.053831)) * sin(radians(lat)))) AS distancia FROM data GROUP BY lat HAVING distancia < 0.2 ORDER BY distancia ASC ")
 dim(data)
 dim(coord_sub_set)
 coord_sub_set
-data1 = data[, 1:2]
 data1
 coord_df <- data.frame(data1, within_5km = geosphere::distHaversine(data1, c(-30.053831, -51.191810)) < 0.500)
 dim(coord_df)
 
+
+
+
+inside.circle = function(center, radius, p)
+{#Begin Function
+  
+  #Error Checks
+  
+  if ((length(center)) !=2) 
+  {stop ("First argument must be a vector of length two.")}
+  
+  if ( radius<=0 )
+  {stop ("Second argument is not a positive number. Radius must be positive.")}
+  
+  if ( (length(radius)) !=1 )
+  {stop ("Second argument must contain only 1 element.") }
+  
+  #Create lengths for later use.
+  p.1 = length(p[,1])
+  x.coord = numeric(p.1)
+  y.coord = numeric(p.1)
+  
+  #Below will calculate the distance using the distance formula and check if it is within the circle. 
+  #It will iterate through p[i,1] and p[i,2] and check the distance across these points and evaluate via
+  # the if() statement.If the if() is true, x.coord and y.coord will take on those points and they will 
+  #represent the points in the circle. 
+  
+  for (i in 1:p.1)
+  {#for
+    #In an x-y Cartesian coordinate system, the circle with centre coordinates (a, b) and radius r is the set 
+    #of all points (x, y) such that:
+    if ((p[i,1] - center[1])^2 + (p[i,2] - center[2])^2 <= radius^2)
+    {#if
+      #Allocatio of points in circle.
+      x.coord[i] = p[i,1]
+      y.coord[i] = p[i,2]
+    }#if
+  }#for
+  
+  
+  #Get valid values and cbind to get vector of points in circle.
+  x.coord = x.coord[x.coord != 0]
+  y.coord = y.coord[y.coord != 0]
+  coordinates = cbind(x.coord,y.coord)
+  
+  #Get plot values and the parameters for the equation of a circle.
+  theta = seq(0,2*pi,length = 2000)
+  a = center[1]
+  b = center[2]
+  x = a + (radius*cos(theta))
+  y = b + (radius*sin(theta))
+  
+  #Plot cirlce and add points 
+  plot(x,y, type = 'l', xlim = c(0,max(p[,1])), ylim = c(0,max(p[,2])), main = "Plot of circle with given parameters", ylab = "Y", xlab = "X")
+  points(p[,1], p[,2], col = "blue")
+  
+  #The points inside the cirlce are red.
+  points(x.coord,y.coord, col = "red")
+  print(coordinates)
+  return(coordinates)
+  
+}#End Function
+
+inside.circle(c(-2.140090, 57.13763), 0.01, coord)
+
+
+
+
+
+setwd("~/OneDrive/r-files/AffinityPropagationClustering/")
+filenames <- list.files(path = "~/OneDrive/r-files/AffinityPropagationClustering/geo/") 
+filenames
+setwd("~/OneDrive/r-files/AffinityPropagationClustering/geo/") 
+data <- do.call("rbind", lapply(filenames, read.csv, header = TRUE, sep = ";")) 
+setwd("~/OneDrive/r-files/AffinityPropagationClustering/")
+head(data)
+names(data)
+coord_sub_set = sqldf("SELECT *, (6371 * acos(cos( radians(-30.053831)) * cos(radians(lat)) * cos(radians(long) - radians(-51.191810)) + sin(radians(-30.053831)) * sin(radians(lat)))) AS distancia FROM data GROUP BY lat HAVING distancia < 0.2 ORDER BY distancia ASC ")
+dim(data)
+dim(coord_sub_set)
+coord_sub_set
